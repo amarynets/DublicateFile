d055e272a0bed181e6c153b634192ce1a3af619d
Andrii Marynets <andrii.marynets@gmail.com>
2016-02-18 23:10:08 +0200

    Scaner is compleate

--- scaner.cpp
+++ scaner.cpp
@@ -13,20 +13,12 @@ Scaner::~Scaner()
 
 QStringList Scaner::scanFolder(const QString & folderName)
 {
-    QDirIterator it(folderName, QDirIterator::Subdirectories);
+    QDirIterator it(folderName, QDir::Files|QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
     QStringList files;
     while(it.hasNext())
     {
-        if(it.fileName() != "." || it.fileName() != "..")
-        {
-            //qDebug() << it.filePath();
-            files << it.filePath();
-            it.next();
-        }
-        else
-        {
-            it.next();
-        }
+        it.next();
+        files << it.filePath();
     }
     return files;
 }
