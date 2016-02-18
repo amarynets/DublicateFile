56d6065159c120f48e7f1cf8cadb9c18bf869296
Andrii Marynets <andrii.marynets@gmail.com>
2016-02-18 21:29:43 +0200

    finish Scaner, maybe

--- scaner.cpp
+++ scaner.cpp
@@ -1,5 +1,7 @@
 #include "scaner.h"
 
+#include <QDir>
+
 Scaner::Scaner()
 {
 }
@@ -10,6 +12,23 @@ Scaner::~Scaner()
 
 QStringList Scaner::scanFolder(const QString & folderName)
 {
+    QDir dir(folderName);
 
+    QStringList files = dir.entryList(QDir::Files);
+    QStringList::iterator it;
+    QStringList dirs = dir.entryList(QDir::Dirs);
+    it = dirs.begin();
+    while(it != dirs.end())
+    {
+        if(*it != "." && *it != "..")
+        {
+            scanFolder(folderName + "/" + *it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+    return files;
 }
 
