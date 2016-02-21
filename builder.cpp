9f10896a070234c8230e30bce3e923ecb318ca21
Andrii Marynets <andrii.marynets@gmail.com>
2016-02-21 10:42:48 +0200

    Change function createDuplicateList

--- builder.cpp
+++ builder.cpp
@@ -2,6 +2,7 @@
 
 #include <QtAlgorithms>
 
+
 Builder::Builder()
 {
 }
@@ -10,11 +11,34 @@ Builder::~Builder()
 {
 }
 
-void Builder::addIfNeadded(const QPair<QString, QString> & input)
+void Builder::addIfUnique(const QPair<QString, QString> & input)
 {
     auto it = qFind(uniqueFiles, input);
     if(it != uniqueFiles.end())
     {
         uniqueFiles.push_back(input);
     }
+}
+
+void Builder::createDuplicateList(const QVector<QPair<QString, QString> > &input)
+{
+    for(auto it : uniqueFiles)
+    {
+        auto dup = qFind(input, it);
+        while(dup != input.end())
+        {
+            bool equalsHash = it.second == dup->second;
+            bool equalsPath = it.first != dup->first;
+            if(equalsPath == true && equalsHash == true)
+            {
+                duplicateFiles.push_back(*dup);
+            }
+        }
+    }
+}
+
+QStringList Builder::takeResult()
+{
+    qSort(duplicateFiles);
+
 }
--- builder.h
+++ builder.h
@@ -2,14 +2,20 @@
 #define BUILDER_H
 
 #include <QStringList>
+#include <QPair>
+#include <QVector>
 
 class Builder
 {
 public:
     Builder();
     ~Builder();
-    void addIfNeadded(const QString& input);
+    void addIfUnique(const QPair<QString, QString> & input);
+    void createDuplicateList(const QVector<QPair<QString, QString>> & input);
     QStringList takeResult();
+private:
+    QVector<QPair<QString, QString>> uniqueFiles;
+    QVector<QPair<QString, QString>> duplicateFiles;
 };
 
 #endif // BUILDER_H
