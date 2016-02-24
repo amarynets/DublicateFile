99ee5ffabccd1021d97646f941483d2868c7ed97
Andrii Marynets <andrii.marynets@gmail.com>
2016-02-24 08:31:21 +0200

    P

--- scaner.cpp
+++ scaner.cpp
@@ -5,15 +5,26 @@
 QMap<qint64, QVector<QString>> Scaner::scanFolder(const QString & folderName)
 {
     QDirIterator it(folderName, QDir::Files|QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
-    QMap<qint64, QVector<QString>> files;
+    QMap<qint64, QVector<QString>> files;    
     while(it.hasNext())
     {
         it.next();
         auto info = it.fileInfo();
         auto size = info.size();
-        QVector<QString> tmp;
-        tmp.push_back(it.filePath());
-        files[size] = tmp;
+        bool isBe = files.contains(size);
+        if(isBe == true)
+        {
+            auto val = files.value(size);
+            //auto tmp = val.toVector();
+            val.push_back(it.filePath());
+            files[size] = val;
+        }
+        else
+        {
+            QVector<QString> elem;
+            elem.push_back(it.filePath());
+            files[size] = elem;
+        }
     }
     return files;
 }
